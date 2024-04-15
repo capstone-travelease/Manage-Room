620664973fe6ffbedf4931074b39c203c8d07059
Jan-La-Ca <134667954+NgocTienWork2002@users.noreply.github.com>
2024-04-15 23:27:31 +0700

    fix: test

--- src/main/java/com/example/manageroom/Controllers/RoomsController.java
+++ src/main/java/com/example/manageroom/Controllers/RoomsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("partner/api/")
+@RequestMapping("partner/api/manage")
 public class RoomsController {
 
    @Autowired
