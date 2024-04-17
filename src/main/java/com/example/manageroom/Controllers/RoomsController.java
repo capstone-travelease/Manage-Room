8c41043f2c05fa187bdbd9b5d8d36d091a515983
Jan-La-Ca <134667954+NgocTienWork2002@users.noreply.github.com>
2024-04-17 15:39:25 +0700

    fix: adding path url

--- src/main/java/com/example/manageroom/Controllers/RoomsController.java
+++ src/main/java/com/example/manageroom/Controllers/RoomsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("partner/api")
+@RequestMapping("/partner/api")
 public class RoomsController {
 
    @Autowired
