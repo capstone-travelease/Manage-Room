d8761c6ab975427170d4d6bc4631ed7583f65a58
Jan-La-Ca <134667954+NgocTienWork2002@users.noreply.github.com>
2024-04-25 21:43:28 +0700

    refactor: change endpoint of api

--- src/main/java/com/example/manageroom/Controllers/RoomsController.java
+++ src/main/java/com/example/manageroom/Controllers/RoomsController.java
@@ -12,31 +12,31 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/partner/api/rooms")
+@RequestMapping("/partner/room-management")
 public class RoomsController {
 
    @Autowired
     private RoomsService roomsService;
 
-    @GetMapping
+    @GetMapping("/rooms")
     public ResponeListRooms listRooms(@RequestParam("hotelId") Integer hotelId, HttpServletResponse response){
         List<ListRooms> rooms = roomsService.listRooms(hotelId);
         return new ResponeListRooms(response.getStatus(),rooms,rooms.size(),"OK");
     }
 
-    @GetMapping("/types")
+    @GetMapping("/rooms/types")
     public ResponeDTO listRoomType(HttpServletResponse response){
         List<Object> roomTypes = roomsService.listRoomTypes();
         return new ResponeDTO(response.getStatus(),roomTypes,"OK");
     }
 
-    @GetMapping("/detail/{id}")
+    @GetMapping("/rooms/detail/{id}")
     public  ResponeDetailRoomDTO getDetailedRoom(@PathVariable("id") Integer roomId){
         return new ResponeDetailRoomDTO(HttpServletResponse.SC_OK,roomsService.getDetailedRoom(roomId),"OK");
 
     }
 
-    @PostMapping
+    @PostMapping("/rooms")
     public ResponeRoomIdDTO addNewRoom(HttpServletResponse response, @RequestBody @Valid AddRoomDTO room){
         Integer roomCode = roomsService.insertRoom(room);
         if(roomCode == 500){
@@ -46,7 +46,7 @@ public class RoomsController {
         return new ResponeRoomIdDTO(response.getStatus(),roomCode,"OK");
     }
 
-    @PostMapping("/images")
+    @PostMapping("/rooms/images")
     public ResponeDTO addRoomImage(HttpServletResponse response, @RequestParam("images") List<MultipartFile> file,@RequestParam("roomId") Integer roomId){
            Integer responseCode = roomsService.insertImage(file,roomId);
            if(responseCode == 500){
@@ -56,7 +56,7 @@ public class RoomsController {
         return new ResponeDTO(response.getStatus(),null,"OK");
     }
 
-    @PutMapping
+    @PutMapping("/rooms")
     public ResponeDTO updateRoom(@RequestBody @Valid RequestUpdateRoomDTO rooms,HttpServletResponse response){
         boolean isCheckErrorRoom = roomsService.updateRoom(rooms);
         if(!isCheckErrorRoom){
@@ -68,7 +68,7 @@ public class RoomsController {
     }
 
 
-    @PutMapping("/image/{id}")
+    @PutMapping("/rooms/image/{id}")
     public ResponeDTO updateImage(@PathVariable("id") Integer roomId,@RequestParam("image") List<MultipartFile> image, @RequestPart("data") List<String> data,HttpServletResponse response){
         Integer httpCode = roomsService.updateRoomImage(roomId,image,data);
         if(httpCode == HttpServletResponse.SC_INTERNAL_SERVER_ERROR){
@@ -77,7 +77,7 @@ public class RoomsController {
         }
         return new ResponeDTO(HttpServletResponse.SC_OK,null,"OK");
     }
-    @DeleteMapping
+    @DeleteMapping("/rooms")
     public ResponeDTO disableRoom(HttpServletResponse response, @RequestBody @Valid DisableRoomDTO rooms ){
         boolean isCheckErrorRoom = roomsService.disableRoom(rooms.getRoomId(),rooms.isStatus());
         if(!isCheckErrorRoom){
